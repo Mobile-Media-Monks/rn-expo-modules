e3e31365c951221af495be0fc82e62851837b602
Esteban Vega <iestebanvega1@gmail.com>
2024-08-22 16:32:06 -0500

    firts setup, add a path alias, set up expo router, set up bottom tabs

---
+++ app/_layout.tsx
@@ -0,0 +1,30 @@
+import { Entypo, Feather } from "@expo/vector-icons";
+import { Stack, Tabs } from "expo-router";
+import { theme } from "@/constants/theme";
+
+export default function Layout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Entypo name="leaf" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
