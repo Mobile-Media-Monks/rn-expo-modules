6b5190e7219a6cdc6ddbe3541b7b3c23963a707a
Esteban Vega <iestebanvega1@gmail.com>
2024-08-23 15:03:29 -0500

    add image and onboarding text

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,10 +1,11 @@
 import { theme } from "@/constants/theme";
-import { View, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button, Text } from "react-native";
 import { useRouter } from "expo-router";
 import { useUserStore } from "@/store/userStore";
 import { PlantlyButton } from "@/components/PlantlyButton";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
+import { PlantlyImage } from "@/components/PlantlyImage";
 
 export default function OnboardingScreen() {
   const router = useRouter();
@@ -23,6 +24,13 @@ export default function OnboardingScreen() {
       style={styles.container}
     >
       <StatusBar style="light" />
+      <View>
+        <Text style={styles.heading}>Plantly</Text>
+        <Text style={styles.tagline}>
+          Keep your plants healthy and hydrated
+        </Text>
+      </View>
+      <PlantlyImage />
       <PlantlyButton title="Let me in!" onPress={handlePress} />
     </LinearGradient>
   );
@@ -31,11 +39,20 @@ export default function OnboardingScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "space-evenly",
     alignItems: "center",
     backgroundColor: theme.colorWhite,
   },
-  text: {
+  heading: {
+    fontSize: 42,
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    marginBottom: 12,
+    textAlign: "center",
+  },
+  tagline: {
     fontSize: 24,
+    color: theme.colorWhite,
+    textAlign: "center",
   },
 });
